0d1cbb72383675ab30b383497b021abeeafc6223
Deron22 <daniel.sraitr.024@pslib.cz>
2024-12-15 20:30:09 +0100

    im amazing

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
         var inputValue = searchdata.value.trim(); // Trim whitespace
         printtex.textContent = inputValue;
         if (inputValue === "zprávy") {
-            printtex.innerHTML = '<p><a href="./zpravy.cz/zpravyhlavnistr.html"><img src="../images/DALL·E 2024-12-07 17.57.16 - A creative and modern logo design for a news platform, featuring a stylized globe with abstract lines to symbolize connectivity, a bold font for the t.webp"></a></p></a></p>';
+            printtex.innerHTML='<p><a href="./zpravy.cz/zpravyhlavnistr.html">zprávy random</a></p></a></p>';
         }
         else {
             console.log("peknej moula");
